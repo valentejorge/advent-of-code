6726fb54917ef598bf22d754606da6ed66fc524b
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2024-12-12 23:37:11 -0300

    refactor: improve day01.py

--- day01/day01.py
+++ day01/day01.py
@@ -1,24 +1,18 @@
-with open('puzzle.txt', 'r') as puzzle:
-    puzzle = puzzle.read()
-    print(f'puzzle:\n{puzzle.splitlines()[:10]}')
-    right = puzzle.split()[1::2]
-    left = puzzle.split()[0::2]
-
-    right_sorted = sorted(right)
-    left_sorted = sorted(left)
-
-    print(f'left:\n{left[0:10]}')
-    print(f'right:\n{right[0:10]}')
-    print(f'left:\n{left_sorted[0:10]}')
-    print(f'right:\n{right_sorted[0:10]}')
+def solve_puzzle(puzzle):
+    right = sorted(puzzle.split()[1::2])
+    left = sorted(puzzle.split()[0::2])
 
     result = []
 
     for i in range(1000):
-        print(i)
         result.append(
-                abs(int(right_sorted[i]) - int(left_sorted[i]))
+                abs(int(right[i]) - int(left[i]))
         )
-    print(result)
-    total_distance = sum(result)
-    print(total_distance)
+
+    return sum(result)
+
+
+with open('puzzle.txt', 'r') as f:
+    puzzle = f.read()
+
+    print(solve_puzzle(puzzle))
